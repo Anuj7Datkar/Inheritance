fe61d7b3a64294962325910da5888247514175de
Anuj7Datkar <anujdatkar@gmail.com>
2026-06-19 12:42:12 +0530

    Add files via upload

---
+++ chef2.py
@@ -0,0 +1,13 @@
+from Chef import Chef
+from chinese_chef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+myChef.make_salad()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+myChineseChef.make_salad()
+myChineseChef.make_chicken()
+myChineseChef.make_fried_rice() 
